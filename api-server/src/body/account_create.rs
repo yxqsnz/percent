350cf70005322cf86be884cd54db492946ddbdc4
yxqsnz <yxqsnz@gmail.com>
2022-12-26 10:51:26 -0300

    test(body): add unit tests

--- api-server/src/body/account_create.rs
+++ api-server/src/body/account_create.rs
@@ -18,3 +18,64 @@ pub struct AccountJoin {
     #[validate(length(min = 8, max = 72))]
     pub password: String,
 }
+
+#[test]
+fn account_create_password_too_small() {
+    assert!(AccountCreate {
+        full_name: None,
+        nick: "me123".into(),
+        password: String::new(),
+    }
+    .validate()
+    .is_err())
+}
+
+#[test]
+fn account_create_nick_too_small() {
+    assert!(AccountCreate {
+        full_name: None,
+        nick: "me".into(),
+        password: String::from("12345678"),
+    }
+    .validate()
+    .is_err())
+}
+
+#[test]
+fn account_join_nick_too_small() {
+    assert!(AccountJoin {
+        nick: "me".into(),
+        password: String::from("12345678"),
+    }
+    .validate()
+    .is_err())
+}
+
+#[test]
+fn account_join_password_too_small() {
+    assert!(AccountJoin {
+        nick: "me123".into(),
+        password: String::new(),
+    }
+    .validate()
+    .is_err())
+}
+#[test]
+fn account_create_valid() {
+    assert!(AccountCreate {
+        full_name: None,
+        nick: "me_at_you".into(),
+        password: String::from("12345678"),
+    }
+    .validate()
+    .is_ok())
+}
+#[test]
+fn account_join_valid() {
+    assert!(AccountJoin {
+        nick: "me_at_you".into(),
+        password: String::from("12345678"),
+    }
+    .validate()
+    .is_ok())
+}
