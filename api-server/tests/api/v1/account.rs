f431563075be8912f2fe1bef9511c67c2f07254f
yxqsnz <yxqsnz@gmail.com>
2022-12-26 10:36:52 -0300

    test: add api/v1/account [GET]

--- api-server/tests/api/v1/account.rs
+++ api-server/tests/api/v1/account.rs
@@ -2,35 +2,58 @@ use api_server::endpoints::route;
 use axum::{
     body::Body,
     http::{self, Request, StatusCode},
+    Router,
 };
 use serde_json::json;
 use sqlx::PgPool;
-use tower::ServiceExt;
+use tower::{Service, ServiceExt};
 use tracing_test::traced_test;
 
-#[sqlx::test]
-#[traced_test]
-async fn register_account(db: PgPool) {
-    sqlx::migrate!("../migrations")
-        .run(&db)
-        .await
-        .expect("Migration failed");
+use crate::{json_body, prepare};
 
-    let app = route(db);
+async fn register(app: &mut Router) {
     let req = Request::builder()
         .uri("/api/v1/account")
         .method(http::Method::POST)
         .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
-        .body(Body::from(
-            json! ({
-                "nick": "test",
-                "password": "ayo12345678"
-            })
-            .to_string(),
-        ))
+        .body(json_body! {
+            "nick": "ypeople",
+            "password": "12345678"
+        })
+        .unwrap();
+
+    let res = app.ready().await.unwrap().call(req).await.unwrap();
+
+    assert_eq!(res.status(), StatusCode::CREATED);
+}
+
+#[sqlx::test]
+#[traced_test]
+async fn register_account(db: PgPool) {
+    prepare(&db).await;
+
+    let mut app = route(db);
+    register(&mut app).await;
+}
+
+#[sqlx::test]
+#[traced_test]
+async fn register_and_login(db: PgPool) {
+    prepare(&db).await;
+    let mut app = route(db);
+    register(&mut app).await;
+
+    let req = Request::builder()
+        .uri("/api/v1/account")
+        .method(http::Method::GET)
+        .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+        .body(json_body! {
+            "nick": "ypeople",
+            "password": "12345678"
+        })
         .unwrap();
 
     let res = app.oneshot(req).await.unwrap();
 
-    assert_eq!(res.status(), StatusCode::CREATED);
+    assert_eq!(res.status(), StatusCode::OK);
 }
--- api-server/tests/mod.rs
+++ api-server/tests/mod.rs
@@ -1,2 +1,18 @@
+use sqlx::PgPool;
+
 mod api;
 mod database;
+
+async fn prepare(db: &PgPool) {
+    sqlx::migrate!("../migrations")
+        .run(db)
+        .await
+        .expect("Migration failed");
+}
+
+#[macro_export]
+macro_rules! json_body {
+    ($($tt:tt)*) => {
+        Body::from(json!({$($tt)*}).to_string())
+    };
+}
