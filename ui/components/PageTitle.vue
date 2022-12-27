ab493298687d616753a41a809bec3a827babb190
yxqsnz <yxqsnz@gmail.com>
2022-12-27 09:53:02 -0300

    fire: bye Nuxt.js

--- ui/components/PageTitle.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <p class="bg-emerald-200 border-slate-700 border-2 border-solid rounded-lg p-2 cursor-default"> Percent </p>
-
-</template>
--- ui/pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="text-slate-200 text-2xl text-center">
-        <h1> A simple Twitter clone. </h1>
-        <h2> Written in Rust 🦀</h2>
-    </div>
-</template>
