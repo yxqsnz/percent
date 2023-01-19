7a7d1c8663e3ec47ea0b7283fedff3d2d9a1ff50
yxqsnz <yxqsnz@gmail.com>
2023-01-19 15:43:42 -0300

    web-ui: fix outline

--- web-ui/components/Register/Field.vue
+++ web-ui/components/Register/Field.vue
@@ -1,6 +1,6 @@
 <template>
   <input
-    class="rounded-lg p-2 bg-slate-800 text-slate-200 border border-slate-700 focus:border-blue-500 focus:ring-blue-500 block w-full outline-0"
+    class="rounded-lg p-2 bg-slate-800 text-slate-200 border border-slate-700 focus:border-blue-500 focus:ring-blue-500 block w-full outline-0 outline-none"
     required
     v-bind="$attrs"
   />
