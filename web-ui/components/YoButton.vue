ab112182d05c3fad2091425c412719cd7ce482ff
yxqsnz <yxqsnz@gmail.com>
2023-01-19 15:45:56 -0300

    web-ui: remove outline from YoButton

--- web-ui/components/YoButton.vue
+++ web-ui/components/YoButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="bg-emerald-200 border-slate-700 border-solid border-2 rounded-lg p-2 text-slate-900"
+    class="bg-emerald-200 border-slate-700 border-solid border-2 rounded-lg p-2 text-slate-900 outline-none outline-0"
     v-bind="$attrs"
   >
     <slot />
