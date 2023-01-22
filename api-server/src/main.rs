a3b4aa3c62f6c5d24ec70ea6107c70d045d77dcf
yxqsnz <yxqsnz@gmail.com>
2023-01-22 11:37:59 -0300

    api-server: drop anyhow

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -1,8 +1,8 @@
 #![deny(clippy::pedantic, clippy::nursery)]
 
-use anyhow::Result;
 use api_server::endpoints;
 use axum::Server;
+use color_eyre::{eyre::Context, Result};
 use dotenvy::dotenv;
 use sqlx::postgres::PgPoolOptions;
 use std::{env::var, net::SocketAddr, str::FromStr};
@@ -12,6 +12,7 @@ use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::Subs
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+    color_eyre::install()?;
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -24,16 +25,24 @@ async fn main() -> Result<()> {
 
     let db = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&var("DATABASE_URL")?)
-        .await?;
+        .connect(&var("DATABASE_URL").context("Please add a $DATABASE_URL env var")?)
+        .await
+        .context("Failed to build database pool")?;
 
-    sqlx::migrate!("../migrations").run(&db).await?;
-    let addr = SocketAddr::from_str(&var("API_URL")?)?;
+    sqlx::migrate!("../migrations")
+        .run(&db)
+        .await
+        .context("Failed to migrate database")?;
+
+    let addr = SocketAddr::from_str(&var("API_URL").context("Please add a $DATABASE_URL env var")?)
+        .context("Failed to generate a valid address")?;
+
     info!("Listening on {addr}");
 
     Server::bind(&addr)
         .serve(endpoints::route(db).into_make_service())
-        .await?;
+        .await
+        .context("Failed to bind to $API_URL")?;
 
     Ok(())
 }
